085fc8e9e23af239d957b1617e43adac6ce74151
Tsung-Ju Lii <usefulalgorithm@gmail.com>
2018-10-26 16:40:29 +0800

    Update setup_new_centos.sh

--- setup_new_centos.sh
+++ setup_new_centos.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 yum install -y sudo epel-release bash-completion vim net-tools wget sysstat git lshw gcc gcc-g++ libstdc++-devel \
-  scl-utils centos-release-scl devtoolset-7-gcc devtoolset-7-gcc-c++ boost;\
+  scl-utils centos-release-scl devtoolset-7 boost;\
 curl -o ~/.git-prompt.sh https://raw.githubusercontent.com/git/git/master/contrib/completion/git-prompt.sh; \
 echo "source ~/.git-prompt.sh" >> ~/.bashrc ;\
 echo "PS1='\u@\$PNAME [\[\e[1;31m\]\$(__git_ps1 \" %s\")\[\e[0m\] \[\e[32m\]\$PWD\[\e[0m\] ]\n$ '" >> ~/.bashrc ;\
