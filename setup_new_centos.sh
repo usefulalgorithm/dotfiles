41d79fff5501ec5cf918835b0ef6f0ef3df88d0a
Tsung-Ju Lii <usefulalgorithm@gmail.com>
2018-10-26 16:29:43 +0800

    Update setup_new_centos.sh
    setup vim

--- setup_new_centos.sh
+++ setup_new_centos.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-yum install -y sudo epel-release bash-completion vim net-tools sysstat git lshw gcc gcc-g++ libstdc++-devel \
+yum install -y sudo epel-release bash-completion vim net-tools wget sysstat git lshw gcc gcc-g++ libstdc++-devel \
   scl-utils centos-release-scl devtoolset-7-gcc devtoolset-7-gcc-c++ boost;\
 curl -o ~/.git-prompt.sh https://raw.githubusercontent.com/git/git/master/contrib/completion/git-prompt.sh; \
 echo "source ~/.git-prompt.sh" >> ~/.bashrc ;\
@@ -9,4 +9,6 @@ git config --global color.ui auto ;\
 scl enable devtoolset-7 bash ;\
 echo "source scl_source enable devtoolset-7" >> ~/.bashrc ;\
 cd /root/ ;\
+wget https://raw.githubusercontent.com/usefulalgorithm/dotfiles/master/vimrc ;\
+mv vimrc ~/.vimrc ;\
 git clone http://192.168.81.8:10080/andylii/ceph-cluster-setup-helpers.git
