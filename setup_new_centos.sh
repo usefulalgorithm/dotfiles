626ef0b4fdb2f398a30ad797bd5f49690b33f044
Tsung-Ju Lii <usefulalgorithm@gmail.com>
2018-10-26 16:11:03 +0800

    Create setup_new_centos.sh

---
+++ setup_new_centos.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+yum install -y sudo epel-release bash-completion vim net-tools sysstat git lshw; \
+curl -o ~/.git-prompt.sh https://raw.githubusercontent.com/git/git/master/contrib/completion/git-prompt.sh; \
+echo "source ~/.git-prompt.sh" >> ~/.bashrc ;\
+echo "PS1='\u@\$PNAME [\[\e[1;31m\]\$(__git_ps1 " %s")\[\e[0m\] \[\e[32m\]\$PWD\[\e[0m\] ]\n$ '" >> ~/.bashrc ;\
+yum install -y htop ;\
+git config --global color.ui auto
